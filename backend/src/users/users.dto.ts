e701c3b5b4063317c71d7b4680c23390cef90f37
Matheus Silveira <Matheus.feques99@gmail.com>
2026-05-07 11:05:16 -0300

    feat: Dtos protegidos

--- backend/src/users/users.dto.ts
+++ backend/src/users/users.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsString,
+  IsUUID,
+  MinLength,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
+
 export class usersDto {
+  @IsUUID()
+  @IsOptional()
   id!: string;
+  @IsString()
+  @MinLength(5)
+  @MaxLength(100)
   name!: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
   email!: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
   password!: string;
 }
